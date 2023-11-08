29403e90813654332bd6d5580d82f8e4aa3b2a6a
arun <arunamballa24@gmail.com>
2023-11-08 14:15:22 -0800

    Pre and Post Increments and Decrements

---
+++ Test3.java
@@ -0,0 +1,11 @@
+public class Test3 {
+    public static void main(String[] args) {
+        
+        int a=10;
+        int b=++a;
+        int c=a++;
+        System.out.println("Pre Increment "+b);
+        System.out.println("Post Increment "+c);
+        System.out.println("A Value "+a);
+    }
+}
