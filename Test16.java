8d8ad5310aed4cc7bbb208b1813a7ec62559752c
arun <arunamballa24@gmail.com>
2023-11-09 17:13:10 -0800

    Final Keyword

---
+++ Test16.java
@@ -0,0 +1,12 @@
+final class A{
+    final public void display(){
+        System.out.println("Showing ");
+    }
+}
+public class Test16 {
+
+    public static void main(String[] args) {
+        A obj=new A();
+        obj.display();
+    }
+}
