c20472ae028cbd1780e5d1f9edb903a1ba799259
arun <arunamballa24@gmail.com>
2023-11-10 17:01:18 -0800

    Static Keyword

---
+++ Test18.java
@@ -0,0 +1,10 @@
+public class Test18 {
+    static int a;
+    static{
+        a=10;
+        System.out.println("Static Block");
+    }
+    public static void main(String[] args) {
+        System.out.println("Main Method is Executing");
+    }
+}
