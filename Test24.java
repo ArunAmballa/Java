1121b984223cb7870dd7cd6ba1624965b0ced42d
arun <arunamballa24@gmail.com>
2023-11-13 19:15:39 -0800

    Enhanced For Loop

---
+++ Test24.java
@@ -0,0 +1,19 @@
+public class Test24 {
+    public static void main(String[] args) 
+    {
+        int []arr=new int[5];
+        for(int ele:arr)
+        {
+            System.out.println(ele);
+        }
+
+        int [][]arr1={{12,3,4},{2,3,45}};
+        for (int a[]:arr1)
+        {
+            for (int el:a)
+            {
+                System.out.println(el);
+            }
+        }
+    }
+}
