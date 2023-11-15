d97500df707b9276b994a73e20c0c76a485b4b1c
arun <arunamballa24@gmail.com>
2023-11-14 20:04:45 -0800

    String Buffer

---
+++ Test26.java
@@ -0,0 +1,8 @@
+public class Test26 {
+    public static void main(String[] args) {
+        
+        StringBuffer s1=new StringBuffer("arun");
+        System.out.println(s1.append("Amballa"));
+        System.out.println(s1.capacity());
+    }
+}
