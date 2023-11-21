024c10e35333d5b9dcfb80ed86657b81d27f2b0f
arun <arunamballa24@gmail.com>
2023-11-20 19:34:13 -0800

    Threads Using Thread Class

---
+++ Test40.java
@@ -0,0 +1,42 @@
+class Music extends Thread
+{
+    public void run()
+    {
+        for (int i=0;i<5;i++)
+        {
+            System.out.println("Playing Music");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
+    }
+}
+class Browsing extends Thread
+{
+    public void run()
+    {
+        for (int i=0;i<5;i++)
+        {
+            System.out.println("Browsing Web");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+public class Test40
+{
+    public static void main(String[] args) 
+    {
+        Music obj=new Music();
+        Browsing obj1=new Browsing();
+        obj.start();
+        obj1.start();
+    }
+    
+}
