9c4c9138ec38e913a1a5e55ac31dddfee1f1862c
arun <arunamballa24@gmail.com>
2023-11-22 15:20:13 -0800

    Priority Queue

---
+++ Test53.java
@@ -0,0 +1,14 @@
+import java.util.PriorityQueue;
+
+public class Test53 
+{
+    public static void main(String[] args) 
+    {
+        PriorityQueue pq=new PriorityQueue<>();
+        pq.add(10);
+        pq.add(200);
+        pq.add(5);
+        pq.add(300);
+        System.out.println(pq);
+    }
+}
