7b416baf717b8c05c1c2c4b7220b4e46cdc16434
arun <arunamballa24@gmail.com>
2023-11-22 18:55:12 -0800

    Interfaces Pracctice

---
+++ Practice4.java
@@ -0,0 +1,49 @@
+interface Calc
+{
+    int a=10;
+    void add();
+    void mul();
+    default public void show()
+    {
+        System.out.println("Show Method of Calc interface");
+    }
+    public static void display()
+    {
+        System.out.println("Stattic Method of Interface");
+    }
+}
+interface CalcPro
+{
+    int a=10;
+    void add();
+    void mul();
+    // default public void show()
+    // {
+    //     System.out.println("Show Method of Calc interface");
+    // }
+}
+class A implements Calc,CalcPro
+{
+    @Override
+    public void add()
+    {
+        System.out.println("Adding");
+    }
+    @Override
+    public void mul()
+    {
+        System.out.println("Multiplyig");
+    }
+}
+public class Practice4 
+{
+    public static void main(String[] args) {
+        
+        A obj=new A();
+        obj.add();
+        obj.mul();
+        obj.show();
+        Calc.display();
+
+    }
+}
