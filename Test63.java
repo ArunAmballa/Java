0196178c614f0ba83bbbdf2a8ae35a52f8702afa
arun <arunamballa24@gmail.com>
2023-11-25 18:09:51 -0800

    Enum in java

---
+++ Test63.java
@@ -0,0 +1,34 @@
+enum Result
+    {
+        PASS,FAIL,NR;
+
+        int marks;
+
+        Result()
+        {
+            System.out.println("Constructor of Enum");
+        }
+
+        void setMarks(int marks)
+        {
+            this.marks=marks;
+        }
+        int getMarks()
+        {
+            return marks;
+        }
+    }
+public class Test63 
+{
+    
+    public static void main(String[] args) 
+    {
+        Result m=Result.PASS;
+        System.out.println(m);
+        Result.PASS.setMarks(40);
+        int m1=Result.PASS.getMarks();
+        System.out.println(m1);
+
+    }
+}
+ 
