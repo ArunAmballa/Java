096e7014ff84803e9729b6dffdfcac633942fd7a
arun <arunamballa24@gmail.com>
2023-11-26 12:03:53 -0800

    New Switch Case

---
+++ Test73.java
@@ -0,0 +1,19 @@
+public class Test73 
+{
+    public static void main(String[] args) 
+    {
+        int x=7;
+
+        switch(x)
+        {
+            case 1->System.out.println("1");
+            case 2->System.out.println("2");
+            case 3->System.out.println("3");
+            case 4->System.out.println("4");
+            case 5->System.out.println("5");
+            case 6->System.out.println("6");
+            default->System.out.println("10");
+        }
+    }
+    
+}
