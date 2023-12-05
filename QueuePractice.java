aaef413bdb0674737bb45a1ec188c03af20430e6
arun <arunamballa24@gmail.com>
2023-12-04 18:42:29 -0800

    Queues  in Java

---
+++ QueuePractice.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueuePractice 
+{
+    public static void main(String[] args) 
+    {
+
+        Queue <Integer> queue=new LinkedList<>();
+        queue.offer(12);
+        queue.offer(14);
+        System.out.println(queue);
+        System.out.println(queue.poll());
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.peek());
+        
+    }
+    
+}
